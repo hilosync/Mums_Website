b43b7c34b158c514694f8ff22632fa2d36e90f71
hilosync <62771262+hilosync@users.noreply.github.com>
2021-11-02 01:16:34 +0000

    Start of mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const hamburgerMenu = document.querySelector("#hamburgerMenu");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+hamburgerMenu.addEventListener("click", function(){
+    console.log("open hamburger menu");
+
+    if(header.classList.contains("open")){
+        header.classList.remove("open");
+        overlay.classList.add("fade-out");
+        overlay.classList.remove("fade-in");
+
+    }
+    else{
+        header.classList.add("open");
+        overlay.classList.add("fade-in");
+        overlay.classList.remove("fade-out");
+
+    }
+
+});
